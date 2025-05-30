0bdf1cadf4e599d4a009b14c4027dd391d46c903
Ubuntu <ubuntu@ip-172-31-3-135.us-east-2.compute.internal>
2025-05-30 08:25:34 +0000

    Add custom admin panel for superusers listing registered users; add nav 'Admin' link for superusers

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -16,6 +18,13 @@ def register(request):
     return render(request, 'accounts/register.html', {'form': form})
 
 
+def home(request):
 @login_required
 def home(request):
     return render(request, 'accounts/home.html')
+@login_required
+def admin_panel(request):
+    if not request.user.is_superuser:
+        return HttpResponseForbidden("You do not have permission to view this page.")
+    users = User.objects.all().order_by('username')
+    return render(request, 'accounts/admin.html', {'users': users})
