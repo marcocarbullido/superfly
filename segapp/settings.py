f6466b1cfa55adfa029e4ceb6317b4e2f5dce500
Ubuntu <ubuntu@ip-172-31-3-135.us-east-2.compute.internal>
2025-05-30 07:41:29 +0000

    Add user registration: accounts app with register, login/logout, home views; update settings and URL configurations

--- segapp/settings.py
+++ segapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "viewer",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "temp"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
